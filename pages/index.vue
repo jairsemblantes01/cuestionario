c13bb61cddfb86e7d5ab2b48032bb8b6d1cfa959
Jair Semblantes <jair_semblantes@hotmail.com>
2023-03-12 18:37:02 -0500

    nuevas preguntas

--- pages/index.vue
+++ pages/index.vue
@@ -205,6 +205,76 @@ export default {
           ],
           respuestaCorrecta: 1
         },
+
+        {
+          pregunta: "En la actualidad los productos o servicios tienen ciclos de vida que se caracterizan por",
+          opciones: [
+            "Ser innovadores y tener ciclos de vida cortos",
+            "El impulso que dan las marcas en diferentes ámbitos como la redes sociales",
+            "Ser innovadores y generar tendencias que persistan en el tiempo",
+            "Tener una alta variabilidad en la demanda"
+          ],
+          respuestaCorrecta: 0,
+        },
+        {
+          pregunta: "En estos últimos años se han desarrollado varias formas de modelos de negocio\
+  ingresos y tipos de e-commerce. En cuanto al primero (modelo de negocio), aquel\
+  relacionado con la distribución de información digital en línea a precios accesibles y\
+  de fácil reproducción en varios dispositivos se relaciona con",
+          opciones: [
+            "Creador de mercado",
+            "E-Tailer",
+            "Proveedor de contenido",
+            "Proveedor de servicios"
+          ],
+          respuestaCorrecta: 2,
+        },
+        {
+          pregunta: "Hay elementos claves para entender el nuevo panorama de e-commerce, estos se\
+  relacionan con",
+          opciones: [
+            "La adaptación, lo social y lo móvil",
+            "Lo social, lo móvil y la ubicación",
+            "La demanda y el abastecimiento",
+            "Lo social, lo oblicuo y la interactividad"
+          ],
+          respuestaCorrecta: 1,
+        },
+        {
+          pregunta: "Según lo estudiado en clase, las empresas usan la tecnología de la información para\
+  responder con rapidez varios elementos de negocios, Excepto",
+          opciones: [
+            "Los cambios en la demanda y preferencia del mercado",
+            "Lograr mayor eficiencia operacional",
+            "Reducir los inventarios a los niveles óptimos posibles",
+            "Controlar el uso de redes sociales para enfocarse en los objetivos estratégicos"
+          ],
+          respuestaCorrecta: 3,
+        },
+        {
+          pregunta: "¿Los sistemas de administración de las relaciones con el cliente nos dice que?",
+          opciones: [
+            "Su principal objetivo es automatizar sus servicios y tener su información en un solo\
+            lugar, esto se logra gracias al uso de sistemas empresariales como lo es un ERP.\
+            También mejorar su toma de decisiones para darle valor competitivo a la empresa.",
+            "Aportan información para coordinar todos los procesos de negocios relacionados con\
+            los clientes en las áreas de ventas, marketing y servicio al cliente, para optimizar los\
+            ingresos, al igual que la satisfacción y la retención del cliente"
+          ],
+          respuestaCorrecta: 1,
+        },
+        {
+          pregunta: "Describa un objetivo específico por lo que las empresas inviernen en un sistema de información",
+          opciones: [
+            "Su principal objetivo es automatizar sus servicios y tener su información en un solo\
+            lugar, esto se logra gracias al uso de sistemas empresariales como lo es un ERP.\
+            También mejorar su toma de decisiones para darle valor competitivo a la empresa.",
+            "Aportan información para coordinar todos los procesos de negocios relacionados con\
+            los clientes en las áreas de ventas, marketing y servicio al cliente, para optimizar los\
+            ingresos, al igual que la satisfacción y la retención del cliente"
+          ],
+          respuestaCorrecta: 0,
+        }
       ],
       respuestas: [],
       calificado: false
